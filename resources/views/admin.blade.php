36bc21413ce86826233cada46aa6954bfc4bd92a
Meir Bensabat <mbensa@carrefourd.local>
2016-10-19 15:32:14 -0400

    Added elements to admin menu for adding goals

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -20,12 +20,11 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">Add a game</div>
                         <div class="panel-body">
-                            <form action="/games" method="POST">
-                                {{ csrf_field() }}
+                            <div class="col-md-6">
                                 <table border="0">
                                     <tr>
                                         <td>Youtube link:</td>
@@ -48,7 +47,7 @@
                                     </tr>
                                     <tr>
                                         <td>Team:</td>
-                                        <td><input type="text" name="vsTeam" /></td>
+                                        <td><input id="vsTeam" type="text" name="vsTeam" /></td>
                                     </tr>
                                     <tr>
                                         <td>Win/Loss:</td>
@@ -56,69 +55,42 @@
                                             <select name="winLoss">
                                                 <option value="W">W</option>
                                                 <option value="L">L</option>
+                                                <option value="T">T</option>
                                             </select>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>Goals for:</td>
-                                        <td><input type="number" name="goalsFor" /></td>
+                                        <td><input id="goalsFor" type="number" name="goalsFor" /></td>
                                     </tr>
                                     <tr>
                                         <td>Goals against:</td>
-                                        <td><input type="number" name="goalsAgainst" /></td>
+                                        <td><input id="goalsAgainst" type="number" name="goalsAgainst" /></td>
                                     </tr>
                                 </table>
+                            </div>
+                            <div class="col-md-3 col-xs-6">
+                                <h4 id="teamHeader">Mighty Pucks</h4>
+                                <ul id="teamList"  class="list-group">
+                                    <li class="list-group-item">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control">
+                                        </div><!-- /input-group -->
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-md-3 col-xs-6">
+                                <h4 id="vsTeamHeader">vs Team</h4>
+                                <ul id="vsTeamList" class="list-group">
+                                    <li class="list-group-item">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control">
+                                        </div><!-- /input-group -->
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">Add goals to game</div>
-                        <div class="panel-body">
-                                <table border="0">
-                                    <tr>
-
-                                    </tr>
-                                    <tr>
-                                        <td>Game date:</td>
-                                        <td><input id="date" type="text" name="date" /></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Season:</td>
-                                        <td>
-                                            <select name="season_id">
-                                                <option selected>Chose season</option>
-                                                @foreach($seasons as $season)
-                                                    <option value="{{ $season->id }}">{{ $season->name }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Team:</td>
-                                        <td><input type="text" name="vsTeam" /></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Win/Loss:</td>
-                                        <td>
-                                            <select name="winLoss">
-                                                <option value="W">W</option>
-                                                <option value="L">L</option>
-                                            </select>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Goals for:</td>
-                                        <td><input type="number" name="goalsFor" /></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Goals against:</td>
-                                        <td><input type="number" name="goalsAgainst" /></td>
-                                    </tr>
-                                </table>
-                                <input type="submit" value="Submit" />
-                            </form>
-                        </div>
+                        <div class="panel-footer"><input class="btn btn-info" type="submit" value="Submit" /></div>
                     </div>
                 </div>
             </div>
@@ -127,7 +99,7 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">Add a user</div>
                         <div class="panel-body">
-                            <div>
+                            <div class="col-md-6">
                                 <form action="/users" method="POST">
                                     {{ csrf_field() }}
                                     <table border="0">
@@ -162,10 +134,10 @@
                                             <td><input type="password" name="sysadminPassword" /></td>
                                         </tr>
                                     </table>
-                                    <input type="submit" value="Submit" />
                                 </form>
                             </div>
                         </div>
+                        <div class="panel-footer"><input class="btn btn-danger" type="submit" value="Submit" /></div>
                     </div>
                 </div>
 
@@ -178,7 +150,62 @@
 @section('js')
     <script>
         $(document).ready(function(){
+
+            var pucksGoals = 0;
+            var vsTeamGoals = 0;
+            var vsTeamName = "";
+
+
             $( "#date" ).datepicker();
+
+
+            //on input change, if number, add the number of boxes to the right for away and home team
+            $("#goalsFor").change(function(){
+                if(this.value >= 0 && this.value <= 99){
+                    items = "";
+                    pucksGoals = parseInt(this.value); //get goals value
+                    $("#teamHeader").empty();
+                    $("#teamHeader").append("Mighty Pucks <span class='badge'>"+ pucksGoals +'</span>'); //add badge to table header team name
+                    for(i = 0; i < pucksGoals; i++){ //create string for adding dynamic amount of inputs for goal times
+                        items += '<li class="list-group-item">' +
+                                    '<div class="input-group">' +
+                                    '<input type="text" class="form-control">' +
+                                    '</div>' +
+                                    '</li>';
+                    }
+                    $("#teamList").empty();
+                    $("#teamList").append(items);
+                }
+
+            });
+
+            $("#goalsAgainst").change(function(){
+                if(this.value >= 0 && this.value <= 99){
+                    vsTeamGoals = parseInt(this.value);
+                    items = "";
+                    $("#vsTeamHeader").empty();
+                    $("#vsTeamHeader").append(vsTeamName + " <span class='badge'>"+ vsTeamGoals +'</span>'); //add badge to table header team name
+                    for(i = 0; i < pucksGoals; i++){ //create string for adding dynamic amount of inputs for goal times
+                        items += '<li class="list-group-item">' +
+                                '<div class="input-group">' +
+                                '<input type="text" class="form-control">' +
+                                '</div>' +
+                                '</li>';
+                    }
+                    console.log("test");
+                    $("#vsTeamList").empty();
+                    $("#vsTeamList").append(items);
+                }
+
+            });
+
+            $("#vsTeam").change(function(){
+                vsTeamName = this.value;
+                $("#vsTeamHeader").html(vsTeamName + " ");
+            });
+
+
+
         });
     </script>
 @endsection
